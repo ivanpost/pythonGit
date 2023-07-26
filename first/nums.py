20249c45840086afad38672b477a996e844e3ec1
stepost11 <stepost11@gmail.com>
2023-07-26 21:37:42 +0400

    добавлен mul

--- first/nums.py
+++ first/nums.py
@@ -6,6 +6,11 @@ text = "new text"
 def minus(a, b):
     return a-b
 
+def mul(a, b):
+    return a*b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(3,3))
