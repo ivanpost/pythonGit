eadfcb15398b96141e4204da80628b7e0220e240
stepost11 <stepost11@gmail.com>
2023-07-30 14:41:26 +0400

    изменены кнопки и отправляются com порты в консоль

--- keysend.py
+++ keysend.py
@@ -1,10 +1,17 @@
 import serial
 import keyboard
+import serial.tools.list_ports
 
-com_port = 'COM5'
+com_port = 'COM3'
 baud_rate = 9600
 
 ser = serial.Serial(com_port, baud_rate)
+# Получение списка доступных COM портов
+ports = serial.tools.list_ports.comports()
+
+# Вывод списка портов
+for port in ports:
+    print(port.device)
 
 
 # Функция для отправки данных в COM-порт
@@ -14,21 +21,21 @@ def send_data(data):
 
 # Функция, вызываемая при нажатии клавиш
 def on_key_press(key):
-    if key.name == 'b':
+    if key.name == '2':
         send_data('B')
-    elif key.name == 'R':
+    elif key.name == '6':
         send_data('R')
-    elif key.name == 'L':
+    elif key.name == '4':
         send_data('L')
-    elif key.name == 'F':
+    elif key.name == '8':
         send_data('F')
-    elif key.name == 'G':
+    elif key.name == '7':
         send_data('G')
-    elif key.name == 'I':
+    elif key.name == '9':
         send_data('I')
-    elif key.name == 'H':
+    elif key.name == '1':
         send_data('H')
-    elif key.name == 'J':
+    elif key.name == '3':
         send_data('J')
 
 
